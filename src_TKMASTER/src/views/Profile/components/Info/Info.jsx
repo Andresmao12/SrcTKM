c37e6beddd13b04bbb4b5aab91aceb63dd0adeee
Mauro <andresmao205@gmail.com>
2024-07-05 09:36:35 -0500

    Account window ready, form, validations and save in localStorage

--- src_TKMASTER/src/views/Profile/components/Info/Info.jsx
+++ src_TKMASTER/src/views/Profile/components/Info/Info.jsx
@@ -1,28 +1,63 @@
 import styles from "./Info.module.css";
 
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const userInfo = "userInfo";
 
 const Info = () => {
-  const { register, setValue, handleSubmit } = useForm();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [userUpdate, setUserUpdate] = useState(false);
+  const [errorForm, setErrorForm] = useState(null);
 
   useEffect(() => {
     const userData = JSON.parse(window.localStorage.getItem(userInfo)) || {};
 
-    setValue("name", userData?.name);
-    setValue("age", userData?.age);
-    setValue("email", userData?.email);
-  }, []);
+    setValue("name", userData.name);
+    setValue("age", userData.age);
+    setValue("email", userData.email);
+  }, [setValue]);
+
+  const handleVlidations = () => {
+    if (errors) {
+      setErrorForm(errors);
+      console.log(errors);
+    }
+  };
 
   const handleFormSubmit = (data) => {
     try {
-      window.localStorage.setItem(userInfo, JSON.stringify(data));
-      console.log("Usuario actualizado: ", data);
+      if (Object.keys(errors).length === 0) {
+        window.localStorage.setItem(userInfo, JSON.stringify(data));
+        console.log("Usuario actualizado: ", data);
+        setUserUpdate(true);
+        setErrorForm(null);
+
+        setTimeout(() => {
+          setUserUpdate(false);
+        }, 5000);
+      }
     } catch (error) {
-      console.log("Error al actualizar usuario");
+      console.log("Error al actualizar usuario", error);
     }
+  };
+
+  const ErrorMessage = ({ message }) => {
+    return <span className={styles.messageError}>{message}</span>;
+  };
+
+  const validation = (value) => {
+    return (
+      errorForm != undefined &&
+      value in errorForm &&
+      `errorForm.${value}` != undefined
+    );
   };
 
   return (
@@ -31,37 +66,67 @@ const Info = () => {
         <label className={styles.label}>
           Name
           <input
+            onKeyUp={handleVlidations}
             type="text"
-            {...register("name", { required: true, min: 1, max: 120 })}
-            className={styles.input}
-            autoComplete="off"
-          />
-        </label>
-        <label className={styles.label}>
-          Email
-          <input
-            type="email"
-            {...register("email", { required: true, min: 1, max: 120 })}
-            className={styles.input}
-            autoComplete="off"
-          />
-        </label>
-        <label className={styles.label}>
-          Age
-          <input
-            type="number"
-            {...register("age", {
-              required: true,
+            {...register("name", {
+              required: "Complete el campo",
               min: 1,
-              max: 120,
-              valueAsNumber: true,
+              max: { value: 100, message: "Demasiados caracteres" },
             })}
             className={styles.input}
             autoComplete="off"
           />
+          {validation("name") ? (
+            <ErrorMessage message={errorForm.name.message} />
+          ) : null}
+        </label>
+        <label className={styles.label}>
+          Email
+          <input
+            onKeyUp={handleVlidations}
+            type="email"
+            {...register("email", {
+              required: "Complete el campo",
+              min: 1,
+              max: { value: 100, message: "Demasiados caracteres" },
+            })}
+            className={styles.input}
+            autoComplete="off"
+          />
+          {validation("email") ? (
+            <ErrorMessage message={errorForm.email.message} />
+          ) : null}
+        </label>
+        <label className={styles.label}>
+          Age
+          <input
+            onKeyUp={handleVlidations}
+            type="number"
+            {...register("age", {
+              required: "Complete el campo",
+              min: 1,
+              max: { value: 120, message: "El limite es 120" },
+              valueAsNumber: "Valor invalido",
+            })}
+            className={styles.input}
+            autoComplete="off"
+          />
+          {validation("age") ? (
+            <ErrorMessage message={errorForm.age.message} />
+          ) : null}
         </label>
 
-        <button className={styles.btnSave}>Save</button>
+        <button
+          type="submit"
+          onClick={handleVlidations}
+          className={styles.btnSave}
+          style={errorForm?.age != null ? { marginTop: "15px" } : {}}
+        >
+          {userUpdate ? (
+            <span className={styles.success}>Usuario actualizado</span>
+          ) : null}
+          Save
+        </button>
       </form>
     </section>
   );
